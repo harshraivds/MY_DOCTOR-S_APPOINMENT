a22a3d430fedae26fbacd76474485ff4aae405bd
Harsh Rai <harshraivds@gmail.com>
2025-12-03 12:25:37 +0530

    In this project i have to create doctor appointment and time slots file

--- doctorservice/src/main/java/com/doctorservice/DoctorserviceApplication.java
+++ doctorservice/src/main/java/com/doctorservice/DoctorserviceApplication.java
@@ -2,8 +2,10 @@ package com.doctorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DoctorserviceApplication {
 
 	public static void main(String[] args) {
--- doctorservice/src/main/java/com/doctorservice/entity/TimeSlots.java
+++ doctorservice/src/main/java/com/doctorservice/entity/TimeSlots.java
@@ -1,4 +1,23 @@
 package com.doctorservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "time_slots")
 public class TimeSlots {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "time")
+    private LocalTime time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_schedule_id")
+    @JsonBackReference
+    private DoctorAppointmentSchedule doctorAppointmentSchedule;
 }
